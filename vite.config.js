9d83fbc7fe2b9b75341a8d1145ab6762ef76ae4d
bonnie <kusasen@gmail.com>
2022-05-18 23:05:18 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import ViteImages from 'vite-plugin-vue-images'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/',
   server: { https: true },
   plugins: [vue() ,mkcert() ,ViteImages()],
   css: {
