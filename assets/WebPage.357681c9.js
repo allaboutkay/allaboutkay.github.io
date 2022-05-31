4d91d0263631560a3f4512f88563166ba5274966
bonnie <kusasen@gmail.com>
2022-05-31 08:57:48 +0800

    Updates

---
+++ assets/WebPage.357681c9.js
@@ -0,0 +1,2 @@
+import{P as e,C as n,n as o}from"./CollectionList.0986f1b7.js";import{d as s}from"./detail_data.f23db0e1.js";import{o as i,c as r,d as t,F as c}from"./index.558bd3aa.js";import"./background-plus.3d2be363.js";const l={data(){return{navigation:o,listData:s.web_data}}},f=Object.assign(l,{setup(d){return(a,p)=>(i(),r(c,null,[t(e,{title:"WEB & UI",msg:`creating websites and web apps that incorporate
+interactive design and modern technologies`}),t(n,{data:a.listData,type:"web"},null,8,["data"])],64))}});export{f as default};
