24596ed119ce5749c9a480483027807724414b3c
bonnie <kusasen@gmail.com>
2022-08-07 22:53:02 +0800

    Updates

--- assets/WebPage.69c8d3e5.js
+++
@@ -1,2 +0,0 @@
-import{P as e,C as n,n as o}from"./CollectionList.69199e07.js";import{d as s}from"./detail_data.d6e28bcf.js";import{o as i,c as r,d as a,F as c}from"./index.bc74e1f2.js";import"./background-plus.3d2be363.js";const l={data(){return{navigation:o,listData:s.web_data}}},b=Object.assign(l,{__name:"WebPage",setup(d){return(t,m)=>(i(),r(c,null,[a(e,{title:"WEB & UI",msg:`creating websites and web apps that incorporate
-interactive design and modern technologies`}),a(n,{data:t.listData,type:"web"},null,8,["data"])],64))}});export{b as default};
